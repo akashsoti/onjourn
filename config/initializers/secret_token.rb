a7e8559db0c9d5f5f487c917edcbc85ee5e07546
Akash Soti <akujgd@gmail.com>
2013-01-11 23:02:26 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onjourn::Application.config.secret_token = '655b0511ef60b296bf6e55b679a0636fee8ae028f7ca100deadada811af6752fb77f57a94125e38ab43deaf7cdd1de6b06f9e95a0a7a83ba82c2de9f212319c7'
